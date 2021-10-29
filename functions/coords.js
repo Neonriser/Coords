37114448db6dad23b4f2422216af7633c9c0d34d
Neonriser <71651648+Neonriser@users.noreply.github.com>
2021-10-29 19:26:13 -0400

    updated x and y consts

--- functions/coords.js
+++ functions/coords.js
@@ -1,8 +1,11 @@
 let crdArray = [0, 0];
 
-exports.handler = async () => {
-  const x = crdArray[0];
-  const y = crdArray[1];
+exports.handler = async (event) => {
+  const x = event.queryStringParameters.lat;
+  const y = event.queryStringParameters.lng;
+
+  crdArray = [x, y];
+
   return {
     statusCode: 200,
     headers: { "Access-Control-Allow-Origin": "*" },
