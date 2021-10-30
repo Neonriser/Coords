1f6d8349818415bbcc28b4f635ffba46adec76f2
Neonriser <71651648+Neonriser@users.noreply.github.com>
2021-10-30 09:44:06 -0400

    Thanks Albert!

--- functions/coords.js
+++ functions/coords.js
@@ -1,8 +1,10 @@
 let crdArray = [0, 0];
 
 exports.handler = async (event) => {
-  const x = event.queryStringParameters.lat;
-  const y = event.queryStringParameters.lng;
+  const x = event.queryStringParameters.lat || crdArray[0];
+  const y = event.queryStringParameters.lng || crdArray[1];
+
+  console.log(`x is ${x}; y is ${y}`);
 
   crdArray[0] = x;
   crdArray[1] = y;
